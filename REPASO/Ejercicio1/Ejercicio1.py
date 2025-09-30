2bf33b850b0ca27695cb2b4fafe7e8346a8c6294
mariarodriguezgarcia89 <rodriguezgarciamaria89@gmail.com>
2025-09-30 12:42:58 +0200

    Create Ejercicio1.py

---
+++ REPASO/Ejercicio1/Ejercicio1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ejercicio1.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
+    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayoutWidget = QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(150, 230, 160, 80))
+        self.vLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.vLayout.setObjectName(u"vLayout")
+        self.vLayout.setContentsMargins(0, 0, 0, 0)
+        self.lblTexto = QLabel(self.verticalLayoutWidget)
+        self.lblTexto.setObjectName(u"lblTexto")
+        self.lblTexto.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+
+        self.vLayout.addWidget(self.lblTexto)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.lblTexto.setText(QCoreApplication.translate("MainWindow", u"ARRANCADO", None))
+    # retranslateUi
+
