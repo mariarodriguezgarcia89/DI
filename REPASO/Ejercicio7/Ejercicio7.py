02e6de42073c58696c3cbd75425c04524bc08f67
mariarodriguezgarcia89 <rodriguezgarciamaria89@gmail.com>
2025-10-07 13:18:23 +0200

    Create Ejercicio7.py

---
+++ REPASO/Ejercicio7/Ejercicio7.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ejercicio7.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDoubleSpinBox, QFormLayout, QLabel,
+    QLineEdit, QMainWindow, QMenuBar, QSizePolicy,
+    QSpinBox, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.formLayoutWidget = QWidget(self.centralwidget)
+        self.formLayoutWidget.setObjectName(u"formLayoutWidget")
+        self.formLayoutWidget.setGeometry(QRect(120, 100, 321, 101))
+        self.formLayout = QFormLayout(self.formLayoutWidget)
+        self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.formLayoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label)
+
+        self.lineEdit = QLineEdit(self.formLayoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lineEdit)
+
+        self.label_2 = QLabel(self.formLayoutWidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.formLayout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.label_2)
+
+        self.label_3 = QLabel(self.formLayoutWidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.formLayout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.label_3)
+
+        self.spinBox = QSpinBox(self.formLayoutWidget)
+        self.spinBox.setObjectName(u"spinBox")
+
+        self.formLayout.setWidget(1, QFormLayout.ItemRole.FieldRole, self.spinBox)
+
+        self.doubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
+        self.doubleSpinBox.setObjectName(u"doubleSpinBox")
+
+        self.formLayout.setWidget(2, QFormLayout.ItemRole.FieldRole, self.doubleSpinBox)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"TEXTO", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"ENTERO", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"DECIMAL", None))
+    # retranslateUi
+
