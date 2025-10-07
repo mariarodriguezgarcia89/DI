ed8728991f1c99b917f200738caec85236b6374e
mariarodriguezgarcia89 <rodriguezgarciamaria89@gmail.com>
2025-10-07 13:15:21 +0200

    Create Ejercicio5_main.py

---
+++ REPASO/Ejercicio5/Ejercicio5_main.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtGui import QBrush, QColor, QConicalGradient
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout
+from ejercicio5 import Ui_MainWindow
+
+class Ventana(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # Centrar el layout verticalmente y horizontalmente
+        widget_central = QWidget()
+        layout_centrado = QVBoxLayout(widget_central)
+        layout_centrado.addStretch()
+        layout_centrado.addWidget(self.horizontalLayoutWidget) 
+        layout_centrado.addStretch()
+
+        # Asignar el layout al centro
+        self.setCentralWidget(widget_central)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = Ventana()
+    w.show()
+    sys.exit(app.exec())
