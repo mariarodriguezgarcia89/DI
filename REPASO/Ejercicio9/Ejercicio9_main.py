ddea34b7f539d215fb99edbacba1e6150cffadc5
mariarodriguezgarcia89 <rodriguezgarciamaria89@gmail.com>
2025-10-09 11:19:01 +0200

    Create Ejercicio9_main.py

---
+++ REPASO/Ejercicio9/Ejercicio9_main.py
@@ -0,0 +1,30 @@
+import sys
+from PySide6.QtGui import QBrush, QColor, QConicalGradient
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout
+from ejercicio9 import Ui_MainWindow
+
+class Ventana(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.actionAbrir.triggered.connect(self.abrir)
+        self.actionGuardar.triggered.connect(self.guardar)
+        self.actionSalir.triggered.connect(self.close)
+
+    def abrir(self):
+        self.actionAbrir.setStatusTip("Abrir un archivo")
+        print("Abrir archivo")
+   
+
+    def guardar(self):
+        self.actionGuardar.setStatusTip("Guardar el archivo")
+        print("Guardar archivo")
+    
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = Ventana()
+    w.show()
+    sys.exit(app.exec())
